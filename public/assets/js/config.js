624edefdf704239d3aa25c44987742d588d8d619
Tsarandro Paul <tsarandropaul97@gmail.com>
2026-02-23 06:57:44 +0100

    FIx theme

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -5,7 +5,7 @@
 /* -------------------------------------------------------------------------- */
 var CONFIG = {
   isNavbarVerticalCollapsed: false,
-  theme: 'light',
+  theme: 'dark',
   isRTL: false,
   isFluid: false,
   navbarStyle: 'transparent',
